0a4dbd33dec426d03bf5e796c52e9a32ebfd1afe
LucianocLopes <luciano_clopes@hotmail.com>
2021-12-20 22:50:00 -0300

    Inserindo o primeiro exercício Python

---
+++ python/exercicios-01/exercicio-01.py
@@ -0,0 +1,9 @@
+"""
+Faça um programa de leia um número inteiro e imprima
+"""
+
+# Recebendo um numero do usuário e fazendo um cast para inteiro
+num = int(input('Digite um número inteiro: '))
+
+# Imprimindo o numero do usuário
+print(f'O número informado é {num}.')
